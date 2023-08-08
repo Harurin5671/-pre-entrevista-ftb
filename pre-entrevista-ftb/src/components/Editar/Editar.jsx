b1575f927bb451259f06d0c1f9c557d4d598bd2a
Haru <frankcito639@gmail.com>
2023-08-08 10:56:23 -0500

    fixeando cosas

--- pre-entrevista-ftb/src/components/Editar/Editar.jsx
+++ pre-entrevista-ftb/src/components/Editar/Editar.jsx
@@ -21,8 +21,8 @@ export default function Editar({ abrirEditor, cerrarEditor, data, setData }) {
   }
 
   const handleSaveChanges = () => {
-    setData((prevData) =>
-      prevData.map((item) =>
+    setData((prev) =>
+      prev.map((item) =>
         item.codigo_unico === data.codigo_unico ? editedData : item
       )
     )
@@ -30,67 +30,6 @@ export default function Editar({ abrirEditor, cerrarEditor, data, setData }) {
   }
 
   return (
-    // <div className={styles.overlay}>
-    //   <div className={styles.modal}>
-    //     <h2>Editar Operación</h2>
-    //     <button className={styles.btnClose} onClick={cerrarEditor}>
-    //       X
-    //     </button>
-    //     <form className={styles.formEdit}>
-    //       <label>Fecha:</label>
-    //       <input
-    //         type="date"
-    //         name="fecha"
-    //         value={editedData.fecha}
-    //         onChange={handleInputChange}
-    //       />
-    //       <label>Descripción:</label>
-    //       <input
-    //         type="text"
-    //         name="descripcion"
-    //         value={editedData.descripcion}
-    //         onChange={handleInputChange}
-    //       />
-    //       <label>Moneda:</label>
-    //       <select
-    //         name="moneda"
-    //         value={editedData.moneda}
-    //         onChange={handleInputChange}
-    //       >
-    //         <option value="PEN">PEN</option>
-    //         <option value="USD">USD</option>
-    //       </select>
-    //       <label>Monto:</label>
-    //       <input
-    //         type="text"
-    //         name="monto"
-    //         value={editedData.monto}
-    //         onChange={handleInputChange}
-    //       />
-    //       <button onClick={handleSaveChanges}>Guardar Cambios</button>
-    //     </form>
-    //     <div className={styles.loginBox}>
-    //       <h2>Login</h2>
-    //       <form>
-    //         <div className={styles.userBox}>
-    //           <input type="text" name="" required="" />
-    //           <label>Username</label>
-    //         </div>
-    //         <div className={styles.userBox}>
-    //           <input type="password" name="" required="" />
-    //           <label>Password</label>
-    //         </div>
-    //         <a href="#">
-    //           <span></span>
-    //           <span></span>
-    //           <span></span>
-    //           <span></span>
-    //           Submit
-    //         </a>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
     <div className={styles.overlay}>
       <div className={styles.loginBox}>
         <h2>Editar:</h2>
